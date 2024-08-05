4c0a4c5796252e56e618a414187d8d26d8b6fdd2
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-08-05 14:18:08 -0400

    add: cambio origin

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
   })
